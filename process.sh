6e32ae6791d479d6b1afd6ac8ce0817664938a3d
Yitong Wu <wuyt26@outlook.com>
2023-05-11 17:04:57 -0700

    add sh

--- process.sh
+++ process.sh
@@ -1,3 +1,4 @@
+# OS: Ubuntu 21.04
 sudo apt update
 
 # 1. Environment Setup
@@ -12,7 +13,28 @@ cd ./bin
 cd ..
 ./build-system.sh
 
-# 2. Run Application
+# Firewall rule setup
+# <host_ip>:3000: spa
+# <host_ip>:8000: webapp
+# <host_ip>:8082: service_user_auth
+# <host_ip>:8083: service_product
+# <host_ip>:8084: service_prder
+# <host_ip>:8085: inventory
+# <host_ip>:5432: postgre_db
 
+# 2. Run Application (in dev mode)
+cd ./services
+# To start service:
+./start-service.sh
+# To stop service:
+./stop-service.sh
+
+cd ..
+cd ./web
+python3 manage.py runserver 0.0.0.0:8000
+
+cd ..
+cd ./spa
+yarn dev
 
 # 3. Run Entire System
